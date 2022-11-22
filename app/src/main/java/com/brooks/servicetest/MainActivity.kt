46439dd1a388d0775b139facf4b3e46c488b76fa
brooks <myt2000@126.com>
2022-11-22 18:12:12 +0800

    主界面启动和停止Service

--- app/src/main/java/com/brooks/servicetest/MainActivity.kt
+++ app/src/main/java/com/brooks/servicetest/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.brooks.servicetest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        startServiceBtn.setOnClickListener {
+            val intent = Intent(this, MyService::class.java)
+            startService(intent)   // 启动Service
+        }
+        stopServiceBtn.setOnClickListener {
+            val intent = Intent(this, MyService::class.java)
+            stopService(intent)     // 停止Service
+        }
     }
 }

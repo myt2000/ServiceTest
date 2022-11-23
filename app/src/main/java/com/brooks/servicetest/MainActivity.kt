77bbd6b9a19b099e116d2b68eb9c482c42dd0fc7
brooks <myt2000@126.com>
2022-11-23 14:48:26 +0800

    普通Service， 开启，停止Service, Activity与Service的绑定

--- app/src/main/java/com/brooks/servicetest/MainActivity.kt
+++ app/src/main/java/com/brooks/servicetest/MainActivity.kt
@@ -1,21 +1,49 @@
 package com.brooks.servicetest
 
+import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var downloadBinder: MyService.DownloadBinder
+
+    private val connection = object: ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            downloadBinder = service as MyService.DownloadBinder
+            downloadBinder.startDownload()
+            downloadBinder.getProgress()
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         startServiceBtn.setOnClickListener {
             val intent = Intent(this, MyService::class.java)
+//            Log.d("开启服务", "onCreate: 开启服务")
             startService(intent)   // 启动Service
         }
         stopServiceBtn.setOnClickListener {
             val intent = Intent(this, MyService::class.java)
+//            Log.d("停止服务", "onCreate: 停止服务")
             stopService(intent)     // 停止Service
+        }
+        bindServiceBtn.setOnClickListener {
+            val intent = Intent(this, MyService::class.java)
+            bindService(intent, connection, Context.BIND_AUTO_CREATE)
+        }
+        unbindServiceBtn.setOnClickListener {
+            unbindService(connection)
         }
     }
 }
--- app/src/main/java/com/brooks/servicetest/MyService.kt
+++ app/src/main/java/com/brooks/servicetest/MyService.kt
@@ -2,23 +2,45 @@ package com.brooks.servicetest
 
 import android.app.Service
 import android.content.Intent
+import android.os.Binder
 import android.os.IBinder
+import android.util.Log
 
 class MyService : Service() {
 
+    private val mBinder = DownloadBinder()
+
+    class DownloadBinder: Binder() {
+
+        fun  startDownload() {
+            Log.d("MyService", "startDownload: 执行")
+        }
+
+        fun getProgress(): Int {
+            Log.d("MyService", "getProgress: 执行")
+            return 0
+        }
+    }
+
     override fun onBind(intent: Intent): IBinder {
-        TODO("Return the communication channel to the service.")
+        return mBinder
     }
 
     override fun onCreate() {
         super.onCreate()
+        Log.d("MyService", "onCreate： 执行")
+
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Log.d("MyService", "onStartCommand: 执行")
         return super.onStartCommand(intent, flags, startId)
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        Log.d("MyService", "onDestroy: 执行")
     }
+
+
 }

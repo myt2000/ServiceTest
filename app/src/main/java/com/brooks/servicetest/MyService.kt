dde8b9a92cca5dd096705beb57735e4507eb7391
brooks <myt2000@126.com>
2022-11-23 15:09:12 +0800

    将service替换成前台Service

--- app/src/main/java/com/brooks/servicetest/MyService.kt
+++ app/src/main/java/com/brooks/servicetest/MyService.kt
@@ -1,10 +1,17 @@
 package com.brooks.servicetest
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.Service
+import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.os.Binder
+import android.os.Build
 import android.os.IBinder
 import android.util.Log
+import androidx.core.app.NotificationCompat
 
 class MyService : Service() {
 
@@ -29,6 +36,21 @@ class MyService : Service() {
     override fun onCreate() {
         super.onCreate()
         Log.d("MyService", "onCreate： 执行")
+        val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel("my_service", "前台Service通知", NotificationManager.IMPORTANCE_DEFAULT)
+            manager.createNotificationChannel(channel)
+        }
+        val intent = Intent(this, MainActivity::class.java)
+        val pi = PendingIntent.getActivity(this, 0, intent, 0)
+        val notification = NotificationCompat.Builder(this, "my_service")
+            .setContentTitle("This is content title")
+            .setContentText("This is content text")
+            .setSmallIcon(R.drawable.small_icon)
+            .setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.large_icon))
+            .setContentIntent(pi)
+            .build()
+        startForeground(1, notification)
 
     }
 
@@ -41,6 +63,4 @@ class MyService : Service() {
         super.onDestroy()
         Log.d("MyService", "onDestroy: 执行")
     }
-
-
 }

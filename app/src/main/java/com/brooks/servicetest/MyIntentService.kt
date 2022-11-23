e23962cf4160f034a9705050fe63ab7c09626f13
brooks <myt2000@126.com>
2022-11-23 17:57:06 +0800

    改成JobService,还是跟Service一样

--- app/src/main/java/com/brooks/servicetest/MyIntentService.kt
+++ app/src/main/java/com/brooks/servicetest/MyIntentService.kt
@@ -2,7 +2,9 @@ package com.brooks.servicetest
 
 import android.app.job.JobParameters
 import android.app.job.JobService
+import android.content.Intent
 import android.util.Log
+import kotlin.concurrent.thread
 
 class MyIntentService : JobService() {
     override fun onDestroy() {
@@ -11,12 +13,30 @@ class MyIntentService : JobService() {
     }
 
     override fun onStartJob(params: JobParameters?): Boolean {
-        Log.d("JobService", "onStartJob: ${Thread.currentThread().name}")
+        Log.d("JobService", "onStartJob: ${Thread.currentThread().name}, ${Thread.currentThread().id}")
+        thread {
+            Log.d("JobService", "onStartJob: ${Thread.currentThread().name}, ${Thread.currentThread().id}")
+        }
         return true
     }
 
     override fun onStopJob(params: JobParameters?): Boolean {
-        Log.d("JobService", "onStartJob: ${Thread.currentThread().name}")
+        Log.d("JobService", "onStopJob: ${Thread.currentThread().name}, ${Thread.currentThread().id}")
         return true
     }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Log.d("JobService", "onStartCommand: ${Thread.currentThread().name}, ${Thread.currentThread().id}")
+        thread {
+            Log.d("JobService", "onStartCommand: ${Thread.currentThread().name}, ${Thread.currentThread().id}")
+            stopSelf()
+        }
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        Log.d("JobService", "onCreate: ${Thread.currentThread().name}, ${Thread.currentThread().id}")
+    }
+
 }
